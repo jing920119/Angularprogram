d4d3087c385fd9d60fa3eeb9d256a3fed0e57dab
MSI\jing <jing920119@gmail.com>
2025-09-11 15:35:20 +0800

    改列表

--- src/app/listquestion/listquestion.component.ts
+++ src/app/listquestion/listquestion.component.ts
@@ -12,7 +12,7 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 })
 export class ListquestionComponent {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'stage', 'begintime', 'endtime', 'result'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -28,29 +28,31 @@ export class ListquestionComponent {
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  stage: string;
+  begintime: string;
+  endtime: string;
+  result: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+  { position: 1, name: 'Hydrogen', stage: 'Run', begintime: '2025/09/11', endtime: '2025/09/20', result: 'H' },
+  { position: 2, name: 'Helium', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'He' },
+  { position: 3, name: 'Lithium', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Li' },
+  { position: 4, name: 'Beryllium', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Be' },
+  { position: 5, name: 'Boron', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'B' },
+  { position: 6, name: 'Carbon', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'C' },
+  { position: 7, name: 'Nitrogen', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'N' },
+  { position: 8, name: 'Oxygen', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'O' },
+  { position: 9, name: 'Fluorine', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'F' },
+  { position: 10, name: 'Neon', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Ne' },
+  { position: 11, name: 'Sodium', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Na' },
+  { position: 12, name: 'Magnesium', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Mg' },
+  { position: 13, name: 'Aluminum', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Al' },
+  { position: 14, name: 'Silicon', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Si' },
+  { position: 15, name: 'Phosphorus', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'P' },
+  { position: 16, name: 'Sulfur', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'S' },
+  { position: 17, name: 'Chlorine', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Cl' },
+  { position: 18, name: 'Argon', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Ar' },
+  { position: 19, name: 'Potassium', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'K' },
+  { position: 20, name: 'Calcium', stage: 'End', begintime: '2025/09/11', endtime: '2025/09/20', result: 'Ca' },
 ];
