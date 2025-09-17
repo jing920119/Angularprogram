43c87e19b3581aa3406f7ff158be182c06697909
MSI\jing <jing920119@gmail.com>
2025-09-17 16:57:10 +0800

    09/17-2

--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FloatLabelType, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-questionnaire',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
   templateUrl: './questionnaire.component.html',
   styleUrl: './questionnaire.component.scss'
 })
 export class QuestionnaireComponent {
+  readonly hideRequiredControl = new FormControl(false);
+  readonly floatLabelControl = new FormControl('auto' as FloatLabelType);
 
-  
+  readonly options = inject(FormBuilder).group({
+    hideRequired: this.hideRequiredControl,
+    floatLabel: this.floatLabelControl,
+  });
+  protected readonly floatLabel = toSignal(
+    this.floatLabelControl.valueChanges.pipe(map(v => v || 'auto')),
+    { initialValue: 'auto' },
+  );
+
+  backBtn(): void {
+    // 這裡可以導向註冊頁面，或開啟註冊表單
+    console.log('使用者點擊了登入');
+    // 例如：this.router.navigate(['/register']);
+  }
+  nextBtn(): void {
+    // 這裡可以導向註冊頁面，或開啟註冊表單
+    console.log('使用者點擊了登入');
+    // 例如：this.router.navigate(['/register']);
+  }
 }
